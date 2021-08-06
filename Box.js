15074bb12d8025681a148499721db2f67f95dc54
AniketGuha <83209322+AniketGuha@users.noreply.github.com>
2021-08-06 13:20:21 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,11 @@
+class Box extends Base{
+    constructor(x,y,width,height){
+
+        super(x,y,width,height)
+
+        this.image=loadImage("sprites/wood1.png")
+        }
+
+}//push and pop
+// push applies to box object
+// pop applies to rest of the objects 
